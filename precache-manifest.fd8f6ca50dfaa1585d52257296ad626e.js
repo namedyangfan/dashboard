2af863e651db96bdaff372bfc2d7a8aab283d2a6
Fan Yang <cedric.yf@gmail.com>
2019-01-11 12:09:41 -0500

    Updates

---
+++ precache-manifest.fd8f6ca50dfaa1585d52257296ad626e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/dashboard/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "81312d4a81e499095a3d",
+    "url": "/dashboard/static/js/runtime~main.81312d4a.js"
+  },
+  {
+    "revision": "a23e766a18baa87d0423",
+    "url": "/dashboard/static/js/main.a23e766a.chunk.js"
+  },
+  {
+    "revision": "5f093dbd9e678674eacc",
+    "url": "/dashboard/static/js/1.5f093dbd.chunk.js"
+  },
+  {
+    "revision": "a23e766a18baa87d0423",
+    "url": "/dashboard/static/css/main.811f6565.chunk.css"
+  },
+  {
+    "revision": "5f093dbd9e678674eacc",
+    "url": "/dashboard/static/css/1.a6fa268d.chunk.css"
+  },
+  {
+    "revision": "8362e7c07c59d0b8cb37f889bb50fa49",
+    "url": "/dashboard/index.html"
+  }
+];
