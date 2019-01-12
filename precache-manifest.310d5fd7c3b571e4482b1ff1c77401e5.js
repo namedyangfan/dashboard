3a95daa7f64c3825a18915f3ee22cf3a1eae5ef7
Fan Yang <cedric.yf@gmail.com>
2019-01-12 16:52:45 -0500

    Updates

---
+++ precache-manifest.310d5fd7c3b571e4482b1ff1c77401e5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/dashboard/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "81312d4a81e499095a3d",
+    "url": "/dashboard/static/js/runtime~main.81312d4a.js"
+  },
+  {
+    "revision": "0808d898ffdc5a136458",
+    "url": "/dashboard/static/js/main.0808d898.chunk.js"
+  },
+  {
+    "revision": "f487a6648b6ec11b684e",
+    "url": "/dashboard/static/js/1.f487a664.chunk.js"
+  },
+  {
+    "revision": "0808d898ffdc5a136458",
+    "url": "/dashboard/static/css/main.f28752a9.chunk.css"
+  },
+  {
+    "revision": "f487a6648b6ec11b684e",
+    "url": "/dashboard/static/css/1.d6367bc1.chunk.css"
+  },
+  {
+    "revision": "20964544d1521cc4d9e59a1b9f237aed",
+    "url": "/dashboard/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/dashboard/precache-manifest.c03c9aa4e54c2faaeafc65d4194a7645.js"
+  "/dashboard/precache-manifest.310d5fd7c3b571e4482b1ff1c77401e5.js"
 );
 
 workbox.clientsClaim();
