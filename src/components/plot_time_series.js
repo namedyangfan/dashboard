ec803fca5d3766ccbd4be086103a75505de4104f
fan <cedric.yf@gmail.com>
2020-06-12 11:40:07 -0400

    refactor plot time series using react hook

--- src/components/plot_time_series.js
+++ src/components/plot_time_series.js
@@ -3,50 +3,48 @@ import axios from 'axios';
 import _ from 'lodash';
 import Plot from 'react-plotly.js';
 
-
-export default class PlotTimeSeries extends React.Component {
-
-  renderPlotFlow = () => {
+const PlotTimeSeries = ({ value, date }) => {
+  const renderPlotFlow = () => {
     const layout = {
       responsive: true,
       xaxis: {
-           title: 'Date',
-           titlefont: {
-               family: 'Courier New, monospace',
-               size: 18,
-               color: '#7f7f7f'
-           }
+        title: 'Date',
+        titlefont: {
+          family: 'Courier New, monospace',
+          size: 18,
+          color: '#7f7f7f',
+        },
       },
       yaxis: {
-           title: 'Discharge Rate ft3/s',
-           automargin: true,
-           titlefont: {
-               family: 'Courier New, monospace',
-               size: 18,
-               color: '#7f7f7f'
-           }
+        title: 'Discharge Rate ft3/s',
+        automargin: true,
+        titlefont: {
+          family: 'Courier New, monospace',
+          size: 18,
+          color: '#7f7f7f',
+        },
       },
-
-    }
+    };
 
     const data = {
-      y: this.props.value,
-      x: this.props.date,
+      y: value,
+      x: date,
       type: 'scatter',
       mode: 'lines+points',
-      marker: {color: 'red'}
-    }
+      marker: { color: 'red' },
+    };
 
-    return(
-      <Plot data = {[ data ]} layout = { layout } className="plotly-plot"
-        useResizeHandler = 'true'
+    return (
+      <Plot
+        data={[data]}
+        layout={layout}
+        className='plotly-plot'
+        useResizeHandler='true'
       />
     );
-  }
+  };
 
-  render() {
-    return (
-      this.renderPlotFlow()
-    )
-  }
-}
+  return renderPlotFlow();
+};
+
+export default PlotTimeSeries
